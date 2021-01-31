139bd2db40bcbf9bf14eeca89b16543e7993343e
Luiz Philippe <luizphilippep@gmail.com>
2021-01-31 15:44:02 -0300

    Prefix tree interface fix

--- include/tree/prefix-tree.hpp
+++ include/tree/prefix-tree.hpp
@@ -11,20 +11,15 @@ namespace tree{
 
         private:
             int size = 0;
-            int height = 0;
-            int leaves = 0;
             Node* root = nullptr;
             
         public:
             PrefixTree();
             ~PrefixTree();
             int get_size(); // O(1)
-            int get_height(); // O(1)
-            void add(stdd::string object);
             Node* get_root(); // O(1)
-            void each(void (*callback)(std::string));
-            void each(void (*callback)(std::string, char*));
-            void remove(Node*); // O(1)
+            Node* find(int code);
+            void add(Node* node, std::string object);
             void clear();
     };
 }
